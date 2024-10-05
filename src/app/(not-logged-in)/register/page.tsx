5cc4918b8c1c5acf41889e0a76f3f57fd73093ee
nhanaa <nhanpro7d@gmail.com>
2024-10-04 19:05:17 -0500

    Create registration general page

---
+++ src/app/(not-logged-in)/register/page.tsx
@@ -0,0 +1,25 @@
+import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <Box
+      display="flex"
+      height="full"
+      width="full"
+      justifyContent="center"
+      alignItems="center"
+      flexGrow={1}
+    >
+      <Stack alignItems="center">
+        <Text fontSize="3xl">Who are you registering as?</Text>
+        <Link href="/register/teacher">
+          <Button colorScheme="red" width={96}>Teacher</Button>
+        </Link>
+        <Link href="/register/student">
+          <Button colorScheme="red" width={96}>Crimson Defense Member</Button>
+        </Link>
+      </Stack>
+    </Box>
+  );
+}
