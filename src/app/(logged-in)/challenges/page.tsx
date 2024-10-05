2a6e1e33d0b52334dac65d34bbd6c39e5e9e1b1e
GoZaddy <faruqyusuff437@gmail.com>
2024-10-05 04:14:41 -0500

    get all challenges instead of just challenges created in current year

--- src/app/(logged-in)/challenges/page.tsx
+++ src/app/(logged-in)/challenges/page.tsx
@@ -5,8 +5,7 @@ import ChallengesTable from "../../../components/ChallengesTable";
 import { useGetChallenges } from "@/hooks/challenges.hooks";
 
 export default function Page() {
-  const curr_year = new Date().getFullYear();
-  const {isPending, error, data} = useGetChallenges(curr_year);
+  const {isPending, error, data} = useGetChallenges();
 
   if (isPending) return <div className="flex justify-center items-center h-100 text-bold">Loading...</div>;
   if (error) return <div className="flex justify-center items-center h-100 text-bold">Error: {error.message}</div>;
