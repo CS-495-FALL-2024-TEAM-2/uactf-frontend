91dbc4027951a7c267dcd9cdd38270d04630366f
GoZaddy <faruqyusuff437@gmail.com>
2024-10-19 16:47:53 -0500

    split navbar into different navbars for the different user roles

---
+++ src/components/nav-bar/AdminNavbar.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useColorModeValue,
+  Stack,
+  Center,
+  IconButton,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import Link from 'next/link'
+import NavbarLogo from './NavbarLogo'
+import NavbarThemeMenuItem from './NavbarThemeMenuItem'
+
+export default function AdminNavbar() {
+  const profileMenu = <Menu>
+  <MenuButton
+    as={Button}
+    rounded={'full'}
+    cursor={'pointer'}
+    minW={0}
+    size={'sm'}>
+    {/* TODO: Log In/Register when we have auth setup */}
+    My Profile
+  </MenuButton>
+  <MenuList alignItems={'center'}>
+    <br />
+    <Center>
+      <Avatar
+        size={'2xl'}
+        src={'https://avatars.dicebear.com/api/male/username.svg'}
+      />
+    </Center>
+    <br />
+    <Center>
+      <p>Username</p>
+    </Center>
+    <br />
+    <MenuDivider />
+    <MenuItem>Your Servers</MenuItem>
+    <MenuItem>Account Settings</MenuItem>
+    <MenuItem>Logout</MenuItem>
+  </MenuList>
+</Menu>;
+
+
+
+  return (
+    <>
+      <Box bg={useColorModeValue('bama_gray', 'black')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+
+          <NavbarLogo />
+
+          <Box className='md:hidden'>
+            <div className='inline mr-4'>
+              {profileMenu}
+            </div>
+            <div className='inline mr-4'>
+            <NavbarThemeMenuItem />
+            </div>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label='Navbar'
+                icon={<HamburgerIcon />}
+                variant='outline'
+              />
+              <MenuList>
+                <MenuItem>
+                  <Link href="/challenges">
+                    Challenges
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  Teams
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+
+
+          <Box className='hidden md:block'>
+            <Flex alignItems={'center'}>
+              <Stack direction={'row'} spacing={3}>
+                <Link href="/challenges">
+                  <Button size='sm' bg={useColorModeValue('bama_gray', 'black')}>
+                    Challenges
+                  </Button>
+                </Link>
+                <Button size='sm' bg={useColorModeValue('bama_gray', 'black')}>
+                  Teams
+                </Button>
+                <NavbarThemeMenuItem />
+
+                {profileMenu}
+              </Stack>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  )
+}
