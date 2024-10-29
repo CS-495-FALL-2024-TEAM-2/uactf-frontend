9b915130bc004f1e528be1d3a667d694da842697
GoZaddy <faruqyusuff437@gmail.com>
2024-10-29 11:17:26 -0500

    integrated backend for login functionality

--- src/hooks/accounts.hooks.ts
+++ src/hooks/accounts.hooks.ts
@@ -1,4 +1,4 @@
-import { CreateCrimsonDefenseRequest, CreateTeacherRequest } from '@/types/accounts.types';
+import { CreateCrimsonDefenseRequest, CreateTeacherRequest, LoginRequest } from '@/types/accounts.types';
 import { BASE_API_URI } from '@/utils/constants';
 import { UseMutateFunction, useMutation } from '@tanstack/react-query';
 
@@ -73,3 +73,39 @@ export const useCreateTeacher = (
 
   return {mutate: createTeacherMutation.mutate, isPending: createTeacherMutation.isPending};
 }
+
+export const useLogin = (
+    onSuccessFn?: ((data: any, variables: LoginRequest, context: unknown) => Promise<unknown> | unknown) | undefined,
+    onErrorFn?: ((error: Error, variables: LoginRequest, context: unknown) => Promise<unknown> | unknown) | undefined
+) : {
+    isPending: boolean
+    mutate: UseMutateFunction<any, Error, LoginRequest, unknown>
+} => {
+    const login = async (request_body: LoginRequest) => {
+        const response = await fetch(
+            `${BASE_API_URI}/auth/login`, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(request_body),
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Error logging in. Please try again');
+        }
+
+        return await response.json();
+    };
+
+    // mutation
+    const loginMutation = useMutation({
+        mutationFn: login,
+        onSuccess: onSuccessFn,
+        onError: onErrorFn,
+    });
+
+    return {mutate: loginMutation.mutate, isPending: loginMutation.isPending};
+};
