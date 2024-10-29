2284aec918a79eeab71d1923c0f195ccceb6d329
GoZaddy <faruqyusuff437@gmail.com>
2024-10-28 23:05:57 -0500

    fake commit

--- src/hooks/routes.hooks.ts
+++ src/hooks/routes.hooks.ts
@@ -1,26 +1,34 @@
 "use client"
 import { usePathname } from "next/navigation";
 import { useGetCurrentUser } from "@/contexts/current-user.context";
+import { useEffect, useState } from "react";
 
 export function useProtectedRoute() {
     const currentUser = useGetCurrentUser();
-
     const pathname = usePathname();
 
+    let result = false;
 
-    if (pathname.startsWith("/competitions")){
-        if (currentUser?.userRole === "admin"){
-            return true;
-        }
-    } else if (pathname.startsWith("/challenges")){
-        if (currentUser?.userRole === "admin" || currentUser?.userRole === "uacd"){
-            return true;
-        } 
-    } else if (pathname.startsWith("/teams")){
-        if (currentUser?.userRole === "admin" || currentUser?.userRole === "teacher"){
-            return true;
-        }
-    }
 
-    return false;
+    useEffect(() => {
+        if (currentUser !== null){
+            if (pathname.startsWith("/competitions")){
+                if (currentUser?.userRole === "admin"){
+                    result = true;
+                }
+            } else if (pathname.startsWith("/challenges")){
+                if (currentUser?.userRole === "admin" || currentUser?.userRole === "uacd"){
+                    result = true;
+                } 
+            } else if (pathname.startsWith("/teams")){
+                if (currentUser?.userRole === "admin" || currentUser?.userRole === "teacher"){
+                    result = true;
+                }
+            }
+        }
+    }, []);
+
+
+
+    return result;
 }
