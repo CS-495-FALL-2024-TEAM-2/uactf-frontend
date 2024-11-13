a938ef57d77f7a04396cb63f4fa3dc1f1b3581d9
nhanaa <nhanpro7d@gmail.com>
2024-11-13 10:25:16 -0600

    Make body height full screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="h-screen">
         <Provider>{children}</Provider>
       </body>
     </html>
