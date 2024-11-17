1501475e37e7fe61c8f5fc769d8e83167f0d4df5
Faruq Yusuff <faruqyusuff437@gmail.com>
2024-11-17 16:45:17 -0600

    users can now attach file to challenges (#78)

--- src/components/NextLineInfoDisplay.tsx
+++ src/components/NextLineInfoDisplay.tsx
@@ -1,10 +1,10 @@
 import { Box, Text } from "@chakra-ui/react";
 
-export default function NextLineInfoDisplay({heading, data}: {heading: string, data: string}) {
+export default function NextLineInfoDisplay({heading, data, children}: {heading: string, data?: string, children?:React.ReactNode}) {
     return <>
     <Box className="mb-6">
         <Text className="mb-2" as="b">{heading}:</Text>
-        <Text className="mb-2">{data}</Text>
+        {children ? <Box>{children}</Box> : <Text className="mb-2">{data}</Text>}
     </Box>
     </>
 }
